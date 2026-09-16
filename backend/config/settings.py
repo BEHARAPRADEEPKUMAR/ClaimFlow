d28a392effbb650c395f0e8723ad8d4b0aae13ab
Pradeep Kumar <beharapradeepkumar8@gmail.com>
2026-09-16 09:23:44 +0530

    Prepare backend for production deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -108,46 +108,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASE_URL = config(
-    "DATABASE_URL",
-    default=""
-)
-
+DATABASE_URL = config("DATABASE_URL", default="")
 
 if DATABASE_URL:
-
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": DATABASE_URL,
-        }
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=not DEBUG,
+        )
     }
-
 else:
-
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": config(
-                "DB_NAME",
-                default="claimflow"
-            ),
-            "USER": config(
-                "DB_USER",
-                default="postgres"
-            ),
-            "PASSWORD": config(
-                "DB_PASSWORD",
-                default=""
-            ),
-            "HOST": config(
-                "DB_HOST",
-                default="localhost"
-            ),
-            "PORT": config(
-                "DB_PORT",
-                default="5432"
-            ),
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 
